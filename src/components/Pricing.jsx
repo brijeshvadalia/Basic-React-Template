f02eff48937eaf6d55372478561c58f78a523a81
B-8128 <brijeshvadaliya1@gmail.com>
2022-11-03 16:48:40 +0530

    Initial Commit

---
+++ src/components/Pricing.jsx
@@ -0,0 +1,103 @@
+function Pricing() {
+  return (
+    <div class="w-full md:w-2/3 grid grid-cols-1 md:grid-cols-2 gap-6 mx-auto">
+    <div class="shadow p-5 rounded-lg border-t-4 border-green-400 bg-white">
+      <p class="uppercase text-sm font-medium text-gray-500">
+        Starter
+      </p>
+  
+      <p class="mt-4 text-3xl text-gray-700 font-medium">
+        Free
+      </p>
+  
+      <p class="mt-4 font-medium text-gray-700">
+        Up to 5 listing monthly
+      </p>
+  
+      <div class="mt-8">
+        <ul class="grid grid-cols-1 gap-4">
+          <li class="inline-flex items-center text-gray-600">
+            <svg class="w-4 h-4 mr-2 fill-current text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM371.8 211.8l-128 128C238.3 345.3 231.2 348 224 348s-14.34-2.719-19.81-8.188l-64-64c-10.91-10.94-10.91-28.69 0-39.63c10.94-10.94 28.69-10.94 39.63 0L224 280.4l108.2-108.2c10.94-10.94 28.69-10.94 39.63 0C382.7 183.1 382.7 200.9 371.8 211.8z"></path>
+            </svg>
+  
+            20 days only
+          </li>
+  
+          <li class="inline-flex items-center text-gray-600">
+            <svg class="w-4 h-4 mr-2 fill-current text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM371.8 211.8l-128 128C238.3 345.3 231.2 348 224 348s-14.34-2.719-19.81-8.188l-64-64c-10.91-10.94-10.91-28.69 0-39.63c10.94-10.94 28.69-10.94 39.63 0L224 280.4l108.2-108.2c10.94-10.94 28.69-10.94 39.63 0C382.7 183.1 382.7 200.9 371.8 211.8z"></path>
+            </svg>
+  
+            Weekly email newsletter
+          </li>
+  
+          <li class="inline-flex items-center text-gray-600">
+            <svg class="w-4 h-4 mr-2 fill-current text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM371.8 211.8l-128 128C238.3 345.3 231.2 348 224 348s-14.34-2.719-19.81-8.188l-64-64c-10.91-10.94-10.91-28.69 0-39.63c10.94-10.94 28.69-10.94 39.63 0L224 280.4l108.2-108.2c10.94-10.94 28.69-10.94 39.63 0C382.7 183.1 382.7 200.9 371.8 211.8z"></path>
+            </svg>
+  
+            Quota renewed monthly
+          </li>
+        </ul>
+      </div>
+  
+      <div class="mt-8">
+        <button class="bg-gray-400 hover:bg-gray-500 px-3 py-2 rounded-lg w-full text-white">
+          Create Free Listing
+        </button>
+      </div>
+    </div>
+  
+    <div class="shadow p-5 rounded-lg border-t-4 border-green-400 bg-white">
+      <p class="uppercase text-sm font-medium text-gray-500">
+        Standard
+      </p>
+  
+      <p class="mt-4 text-3xl text-gray-700 font-medium">
+        $30 <span class="text-base font-normal">/listing</span>
+      </p>
+  
+      <p class="mt-4 font-medium text-gray-700">
+        When free quota fully used
+      </p>
+  
+      <div class="mt-8">
+        <ul class="grid grid-cols-1 gap-4">
+          <li class="inline-flex items-center text-gray-600">
+            <svg class="w-4 h-4 mr-2 fill-current text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM371.8 211.8l-128 128C238.3 345.3 231.2 348 224 348s-14.34-2.719-19.81-8.188l-64-64c-10.91-10.94-10.91-28.69 0-39.63c10.94-10.94 28.69-10.94 39.63 0L224 280.4l108.2-108.2c10.94-10.94 28.69-10.94 39.63 0C382.7 183.1 382.7 200.9 371.8 211.8z"></path>
+            </svg>
+  
+            30 days only
+          </li>
+  
+          <li class="inline-flex items-center text-gray-600">
+            <svg class="w-4 h-4 mr-2 fill-current text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM371.8 211.8l-128 128C238.3 345.3 231.2 348 224 348s-14.34-2.719-19.81-8.188l-64-64c-10.91-10.94-10.91-28.69 0-39.63c10.94-10.94 28.69-10.94 39.63 0L224 280.4l108.2-108.2c10.94-10.94 28.69-10.94 39.63 0C382.7 183.1 382.7 200.9 371.8 211.8z"></path>
+            </svg>
+  
+            Twice weekly email newsletter
+          </li>
+  
+          <li class="inline-flex items-center text-gray-600">
+            <svg class="w-4 h-4 mr-2 fill-current text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM371.8 211.8l-128 128C238.3 345.3 231.2 348 224 348s-14.34-2.719-19.81-8.188l-64-64c-10.91-10.94-10.91-28.69 0-39.63c10.94-10.94 28.69-10.94 39.63 0L224 280.4l108.2-108.2c10.94-10.94 28.69-10.94 39.63 0C382.7 183.1 382.7 200.9 371.8 211.8z"></path>
+            </svg>
+  
+            Social feed share (3 platforms)
+          </li>
+        </ul>
+      </div>
+  
+      <div class="mt-8">
+        <button class="bg-gray-400 hover:bg-gray-500 px-3 py-2 rounded-lg w-full text-white">
+          Create Standard Listing
+        </button>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Pricing
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import CTA from './components/CTA';
+import Pricing from './components/Pricing';
+import Team from './components/Team';
+import Testimonial from './components/Testimonial';
+import Footer from './components/Footer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('brijesh'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <>
+<Navbar/>
+<Hero/>
+<CTA/>
+<Pricing/>
+<Team/>
+<Testimonial/>
+<Footer/>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+</>
+);
